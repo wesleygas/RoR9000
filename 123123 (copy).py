9a1b56132e064b87322db6e21201ee71b93a815a
brunoartc <brunoartc@yahoo.com>
2018-04-02 15:43:48 -0300

    suvriva

---
+++ 123123 (copy).py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import rospy
+
+import numpy as np
+
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
+
+mini = [10, 0]
+velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+
+def GonnaCrash(mini):
+	return mini[0]<0.3
+def Dont(dire):
+	if (dire==0):
+		velocidade = Twist(Vector3(-2, 0, 0), Vector3(0, 0, 0))
+		velocidade_saida.publish(velocidade)
+		rospy.sleep(0.3)
+	else:
+		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, dire*2))
+		velocidade_saida.publish(velocidade)
+		rospy.sleep(0.1)
+	
+	
+def scaneou(dado):
+	global mini
+	mini = [dado.range_max, 0]
+	lelescan=np.array(dado.ranges).round(decimals=2)
+	for i in range(len(lelescan)):
+		if lelescan[i] >= dado.range_min:
+			pass
+			if mini[0] > lelescan[i]:
+				mini = [lelescan[i],i]
+	print (len(lelescan))
+	#print("Intensities")
+	#print(np.array(dado.intensities).round(decimals=2))
+
+	
+
+
+if __name__=="__main__":
+
+	rospy.init_node("le_scan")
+
+	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+	#print("titi", LaserScan)
+
+
+
+	while not rospy.is_shutdown():
+		#print("Oeee", mini)
+		if GonnaCrash(mini):
+			if (mini[1] < 360 and mini[1] > 320) or (mini[1] < 40 and mini[1] > 0):
+				Dont(0)	
+			if (mini[1] < 360 and mini[1] > 280):
+				Dont(1)
+			elif (mini[1] < 80 and mini[1] > 0):
+				Dont(-1)
+			print("ganna",mini)
+		else:
+			print("ginna",mini)
+			velocidade = Twist(Vector3(2, 0, 0), Vector3(0, 0, 0))
+		velocidade_saida.publish(velocidade)
+		rospy.sleep(0.1)
+
+
--- estados_simples.py
+++ estados_simples.py
@@ -11,18 +11,81 @@ from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import LaserScan
 import smach
 import smach_ros
-import suvriva
+
+
+vel = 0.3
 
 
 limite = 200
 ang_speed = 0.2
+mini = [10, 0]
 
 ## Classes - estados
 """
 Classe simples - estado que gira até o limite depois termina
 """
+
+
+def GonnaCrash(mini):
+	return mini[0]<0.3
+def Dont(dire):
+	global vel
+	global velocidade_saida
+	if (dire==0):
+		velocidade = Twist(Vector3(-2, 0, 0), Vector3(0, 0, 0))
+		print("RE")
+		velocidade_saida.publish(velocidade)
+	else:
+		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, dire*2))
+		velocidade_saida.publish(velocidade)
+	
+	
+def scaneou(dado):
+	global mini
+	mini = [dado.range_max, 0]
+	lelescan=np.array(dado.ranges).round(decimals=2)
+	for i in range(len(lelescan)):
+		if lelescan[i] >= dado.range_min:
+			pass
+			if mini[0] > lelescan[i]:
+				mini = [lelescan[i],i]
+	#print (len(lelescan))
+	#print("Intensities")
+	#print(np.array(dado.intensities).round(decimals=2))
+
+
+
+def normal():
+	global mini
+	global vel
+	if GonnaCrash(mini):
+		return 'algo_errado'
+	else:
+		velocidade = Twist(Vector3(2*0, 0, 0), Vector3(0, 0, 0))
+		print("frente")
+		velocidade_saida.publish(velocidade)
+		return 'OK'
+
+
+def mainn():
+	global mini
+	if (mini[1] < 360 and mini[1] > 320) or (mini[1] < 40 and mini[1] > 0):
+		return 'do_lado_esquerdo'
+	if (mini[1] < 360 and mini[1] > 288):
+		return 'do_lado_direito'
+	elif (mini[1] < 72 and mini[1] > 0):
+		return 'em_frente'
+	return 'OK'
+
+
+
+
+
+
+
 class Girando(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['fim', 'girando'])
@@ -30,52 +93,58 @@ class Girando(smach.State):
 	def execute(self, userdata):
 		self.numero_voltas +=1
 		if self.numero_voltas < limite:
-			vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, -ang_speed))
-			velocidade_saida.publish(vel)
+			#vel = Tiwist(Vector3(0, 0, 0), Vector3(0, 0, -ang_speed))
+			#velocidade_saida.publish(vel)
 			return 'girando'
 		else:
-			vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
-			velocidade_saida.publish(vel)
+			#vel = Tiwist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+			#velocidade_saida.publish(vel)
 			return 'fim'
+
 class Batendo(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['em_frente','OK', 'do_lado_esquerdo', 'do_lado_direito', 'em_frente'])
 	def execute(self, userdata):
-		return suvriva.main()
+		return mainn()
 class Andando(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['OK', 'algo_errado'])
 	def execute(self, userdata):
-		return suvriva.normal()
+		return normal()
 
 class GIRAE(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['em_frente'])
 	def execute(self, userdata):
-		suvriva.Dont(-1)
+		Dont(-1)
 		return 'em_frente'
 
 class GIRAD(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['em_frente'])
 	def execute(self, userdata):
-		suvriva.Dont(1)
+		Dont(1)
 		return 'em_frente'
 
 class RE(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['em_frente'])
 	def execute(self, userdata):
-		suvriva.Dont(0)
+		Dont(0)
 		return 'em_frente'
+
+
+
 
 
 # main
 def main():
 	global velocidade_saida
+	global mini
 	rospy.init_node('unico_estado')
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
 
 	# Cria uma máquina de estados
 	sm = smach.StateMachine(outcomes=['fim_geral'])
@@ -91,15 +160,18 @@ def main():
 			'do_lado_esquerdo':'GIRAD',
 			'do_lado_direito':'GIRAE',
 			'em_frente':'PERIGO'})
-		smach.StateMachine.add('GIRAE', GIRAE(),
+		smach.StateMachine.add('GIRAE', GIRAD(),
 			transitions={'em_frente': 'PERIGO'})
-		smach.StateMachine.add('GIRAD', GIRAD(),
+		smach.StateMachine.add('GIRAD', GIRAE(),
 			transitions={'em_frente': 'PERIGO'})
 		smach.StateMachine.add('RE', RE(),
 			transitions={'em_frente': 'PERIGO'})
 
 	# Executa a máquina de estados
 	outcome = sm.execute()
+
+	vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+	velocidade_saida.publish(vel)
 
 	print("Execute finished")
 
